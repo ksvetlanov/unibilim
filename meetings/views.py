0d98b75c7fbbb43ca83d40cd2c3d4817b3b35aeb
pythonMaks <122172870+pythonMaks@users.noreply.github.com>
2023-07-08 16:37:47 +0600

    Update views.py

--- meetings/views.py
+++ meetings/views.py
@@ -1,7 +1,16 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .models import Meetings
 from .serializers import MeetingsSerializer
 
 class MeetingsViewSet(viewsets.ModelViewSet):
     queryset = Meetings.objects.all()
     serializer_class = MeetingsSerializer
+
+    @action(detail=False, methods=['post'])
+    def by_professor(self, request):
+        professor_id = request.data.get('professor_id')
+        meetings = self.queryset.filter(professor_id=professor_id)
+        serializer = self.get_serializer(meetings, many=True)
+        return Response(serializer.data)
