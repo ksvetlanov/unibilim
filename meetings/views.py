10976e52439464baeb3bb0ff3fde0128f5ff82fa
pythonMaks <python.maxim@yandex.ru>
2023-07-11 11:07:55 +0300

    swagger meetings

--- meetings/views.py
+++ meetings/views.py
@@ -3,19 +3,37 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from .models import Meetings
 from .serializers import MeetingsSerializer
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 class MeetingsViewSet(viewsets.ModelViewSet):
     serializer_class = MeetingsSerializer
 
+    
+    @swagger_auto_schema(manual_parameters=[
+        openapi.Parameter(
+            'professor_id', openapi.IN_QUERY, description="ID of the professor",
+            type=openapi.TYPE_INTEGER),
+        openapi.Parameter(
+            'student_id', openapi.IN_QUERY, description="ID of the student",
+            type=openapi.TYPE_INTEGER),
+        openapi.Parameter(
+            'ordering', openapi.IN_QUERY, description="Order of the results",
+            type=openapi.TYPE_STRING)
+    ])
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+    
     def get_queryset(self):
         queryset = Meetings.objects.all()
         professor_id = self.request.query_params.get('professor_id', None)
         student_id = self.request.query_params.get('student_id', None)
-        
+
         if professor_id is not None:
-            queryset = queryset.filter(professor_id=professor_id)
-            
+            queryset = queryset.filter(professor__id=professor_id)
+
         if student_id is not None:
-            queryset = queryset.filter(student_id=student_id)
-            
+            queryset = queryset.filter(student__id=student_id)
+
         return queryset
+
