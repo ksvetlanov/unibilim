35616c765e5a092fa8ed6c4d9eb92687ec791c45
pythonMaks <python.maxim@yandex.ru>
2023-07-11 10:41:50 +0300

    test script

--- meetings/urls.py
+++ meetings/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import MeetingsViewSet
 
 router = DefaultRouter()
-router.register(r'meetings', MeetingsViewSet)
+router.register(r'meetings', MeetingsViewSet, basename='meetings')
 
 
 urlpatterns = [
