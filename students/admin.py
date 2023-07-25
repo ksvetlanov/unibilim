88e3cf96ee6bcb8ccec802475c5d7e19dbc71b82
AndreyPyBack <125370147+AndreyPyBack@users.noreply.github.com>
2023-07-25 13:15:33 +0300

    Update admin.py
    region

--- students/admin.py
+++ students/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Student
+from .models import Student,District,City,Region
 # Register your models here.
 admin.site.register(Student)
+admin.site.register(Region)
+admin.site.register(District)
+admin.site.register(City)
