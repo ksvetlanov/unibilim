fa82c4b04247a825d39a46591edcdc5de3613903
AndreyPyBack <125370147+AndreyPyBack@users.noreply.github.com>
2023-07-25 13:13:20 +0300

    Update models.py
    region

--- students/models.py
+++ students/models.py
@@ -2,26 +2,42 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Region(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class District(models.Model):
+    name = models.CharField(max_length=100)
+    region = models.ForeignKey(Region, on_delete=models.CASCADE, related_name='districts')
+
+    def __str__(self):
+        return self.name
+
+
+class City(models.Model):
+    name = models.CharField(max_length=100)
+    district = models.ForeignKey(District, on_delete=models.CASCADE, related_name='cities')
+
+    def __str__(self):
+        return self.name
+
+
 class Student(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     firstname = models.CharField(max_length=255)
     surname = models.CharField(max_length=255)
     patronym = models.CharField(max_length=255, blank=True, null=True)
     telegram_username = models.CharField(max_length=255)
-    telegram_id = models.CharField(max_length=255)
+    telegram_id = models.CharField(max_length=255, blank=True, null=True)
     phone_numbers = models.CharField(max_length=20)
     date_of_birth = models.DateField()
-    REGION_CHOICES = [
-        ('region1', 'Край 1'),
-        ('region2', 'Край 2'),
-        ('region3', 'Край 3'),
-        # Добавьте остальные края по аналогии
-    ]
-    region = models.CharField(max_length=255, choices=REGION_CHOICES)
-    city = models.CharField(max_length=255)
-    district_city = models.CharField(max_length=255)  # Добавленное поле
-    status = models.CharField(max_length=255)
-    photo = models.ImageField(upload_to='student_photos', null=True)
+    region = models.ForeignKey(Region, on_delete=models.SET_NULL, blank=True, null=True)
+    city = models.ForeignKey(City, on_delete=models.SET_NULL, blank=True, null=True)
+    district_city = models.ForeignKey(District, on_delete=models.SET_NULL, blank=True, null=True)
+    photo = models.ImageField(upload_to='student_photos', blank=True, null=True)
     otp_token = models.CharField(max_length=255)
     token = models.CharField(max_length=32, null=True)
     language = models.CharField(max_length=50)
