ba2fd4e9298cd383dfb8d9b6d4d3a5ee105f6d89
AndreyPyBack <125370147+AndreyPyBack@users.noreply.github.com>
2023-07-26 20:55:28 +0300

    Update urls.py
    region url

--- students/urls.py
+++ students/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import StudentRegistrationAPIView, OTPVerificationView
+from .views import StudentRegistrationAPIView, OTPVerificationView,RegionListView, DistrictListView, CityListView
 
 # router = DefaultRouter()
 # router.register(r'students', StudentViewSet)
@@ -10,5 +10,8 @@ urlpatterns = [
     # path('', include(router.urls)),
     path('register/student/', StudentRegistrationAPIView.as_view(), name='student_register'),
     path('verify/otp/', OTPVerificationView.as_view(), name='otp_verification'),
+    path('regions/', RegionListView.as_view(), name='region-list'),
+    path('regions/<int:region_id>/districts/', DistrictListView.as_view(), name='district-list'),
+    path('districts/<int:district_id>/cities/', CityListView.as_view(), name='city-list'),
 ]
 
