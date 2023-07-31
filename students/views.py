3eb6e209ccf1092d017cea6ab8aa395c870609b7
AndreyPyBack <125370147+AndreyPyBack@users.noreply.github.com>
2023-07-31 08:44:25 +0300

    Update views.py
    return Response({"message": "Student created successfully."}, status=status.HTTP_201_CREATED)

--- students/views.py
+++ students/views.py
@@ -13,23 +13,21 @@ from .models import Student, District, Region
 from rest_framework import generics
 from .models import Region, District, City
 from .serializers import RegionSerializer, DistrictSerializer, CitySerializer
-from rest_framework.permissions import AllowAny
 
 class RegionListView(generics.ListAPIView):
-    permission_classes = [AllowAny]
     queryset = Region.objects.all()
     serializer_class = RegionSerializer
 
 class DistrictListView(generics.ListAPIView):
     serializer_class = DistrictSerializer
-    permission_classes = [AllowAny]
+
     def get_queryset(self):
         region_id = self.kwargs['region_id']
         return District.objects.filter(region__id=region_id)
 
 class CityListView(generics.ListAPIView):
     serializer_class = CitySerializer
-    permission_classes = [AllowAny]
+
     def get_queryset(self):
         district_id = self.kwargs['district_id']
         return City.objects.filter(district__id=district_id)
@@ -37,8 +35,8 @@ def save_token_to_server(transaction_id, token):
     try:
         student = Student.objects.get(user__username=transaction_id)
         student.otp_token = token
-        print(token)
-        print(student.otp_token)
+        # print(token)
+        # print(student.otp_token)
         student.save()
     except Student.DoesNotExist:
         raise serializers.ValidationError("Неверный идентификатор транзакции")
@@ -71,7 +69,7 @@ def send_otp_code(transaction_id, phone):
 
     if response.status_code == 200 and response_data.get('status') == 0:
         token = response_data.get('token')
-        print(token)
+        # print(token)
         save_token_to_server(transaction_id, token)
         return token
     else:
@@ -102,7 +100,6 @@ def verify_otp_code(token, code):
 
 
 class StudentRegistrationAPIView(APIView):
-    permission_classes = [AllowAny]
     serializer_class = StudentRegisterSerializer
 
 
@@ -127,13 +124,12 @@ class StudentRegistrationAPIView(APIView):
             request.session['transaction_id'] = transaction_id
 
             # Перенаправление на страницу подтверждения кода
-            return redirect('otp_verification')
+            return Response({"message": "Student created successfully."}, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class OTPVerificationView(APIView):
-    permission_classes = [AllowAny]
     serializer_class = OTPVerificationSerializer
 
     def post(self, request):
