a22368aeda909cda59ce4bfe9ee5b5dfbdfae544
AndreyPyBack <125370147+AndreyPyBack@users.noreply.github.com>
2023-08-12 07:43:46 +0300

    Update views.py
    Verification change

--- students/views.py
+++ students/views.py
@@ -100,45 +100,42 @@ def verify_otp_code(token, code):
     else:
         return False
 
-
 class StudentRegistrationAPIView(APIView):
     permission_classes = [AllowAny]
     serializer_class = StudentRegisterSerializer
 
-
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
-
             student = serializer.save()
-
-
             transaction_id = serializer.validated_data['username']
             token = student.otp_token
-            # print("transaction_id:", transaction_id)
-            # print("token:", token)
             save_token_to_server(transaction_id, token)
-
-            # Отправка одноразового пароля
             phone = serializer.validated_data['phone_numbers']
             send_otp_code(transaction_id, phone)
-
-
             request.session['transaction_id'] = transaction_id
 
-            # Перенаправление на страницу подтверждения кода
-            return Response({"message": "Student created successfully."}, status=status.HTTP_201_CREATED)
+            
+            student = Student.objects.get(user__username=transaction_id)
+            token = student.otp_token
+            # Отправить токен вместе с сообщением об успешной регистрации
+            return Response({
+                "message": "Student created successfully.",
+                "token": token  # Включаем токен в ответ
+            }, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class OTPVerificationView(APIView):
     permission_classes = [AllowAny]
-    serializer_class = OTPVerificationSerializer
 
     def post(self, request):
-        serializer = self.serializer_class(data=request.data)
-        serializer.is_valid(raise_exception=True)
+        token = request.data.get('token')
+        code = request.data.get('code')
+
+        if not token or not code:
+            return Response({'message': 'Token and code are required.'}, status=status.HTTP_400_BAD_REQUEST)
 
         url = 'https://smspro.nikita.kg/api/otp/verify'
         api_key = '92503b1a8d4f6e0e30de3cf9e4e35919'
@@ -148,14 +145,13 @@ class OTPVerificationView(APIView):
             'Content-Type': 'application/json'
         }
 
-
         session = SessionStore(request.session.session_key)
         transaction_id = session.get('transaction_id')
-        token = get_token_from_server(transaction_id)
+        token_from_server = get_token_from_server(transaction_id)
 
         data = {
-            'token': token,
-            'code': serializer.validated_data['code']
+            'token': token_from_server,
+            'code': code
         }
 
         response = requests.post(url, headers=headers, json=data)
@@ -164,15 +160,14 @@ class OTPVerificationView(APIView):
 
         if response_data.get('status') == 0 and response_data.get('description') == 'Valid Code':
             transaction_id = session.get('transaction_id')
-            student = Student.objects.get(user__username=transaction_id)  # Находим объект Student по transaction_id
+            student = Student.objects.get(user__username=transaction_id)
             student.status = True
-            student.save()  # Обновляем статус на True
+            student.save()
 
-            return Response({'message': 'Проверка успешна. Статус пользователя обновлен на True.'})
+            return Response({'message': 'Проверка успешна. Статус пользователя обновлен на True.'},
+                            status=status.HTTP_200_OK)
         else:
             return Response({'message': 'Проверка неуспешна'}, status=status.HTTP_400_BAD_REQUEST)
-
-         # return Response(response_data)
 
 
 class CabinetView(generics.RetrieveAPIView):
