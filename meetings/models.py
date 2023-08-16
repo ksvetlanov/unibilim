52d073b93b9b4dda3eaaaa2196e51eba1f6c7554
pythonMaks <122172870+pythonMaks@users.noreply.github.com>
2023-08-16 11:57:32 +0600

    Update models.py

--- meetings/models.py
+++ meetings/models.py
@@ -36,8 +36,11 @@ class Meetings(models.Model):
         # Вернем получившийся хеш в виде строки
         return hash_object.hexdigest()
 
+
+
     def save(self, *args, **kwargs):
-        date_obj = datetime.datetime.strptime(self.datetime, "%Y-%m-%dT%H:%M:%S").date()
+        date_obj = self.datetime.date()
         self.day_of_week = date_obj.strftime('%A')
         self.jitsiLink = "https://meet.jit.si/" + str(self.generate_hash({self.professor.surname},{self.student.surname},{self.datetime}))
         super(Meetings, self).save(*args, **kwargs)
+
