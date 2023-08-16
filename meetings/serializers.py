9245b42a3ef01835d34079dcb08c840b773a6b31
AndreyPyBack <125370147+AndreyPyBack@users.noreply.github.com>
2023-08-16 09:24:46 +0300

    Update serializers.py
    Update serializers.py new

--- meetings/serializers.py
+++ meetings/serializers.py
@@ -2,6 +2,27 @@ from rest_framework import serializers
 from .models import Meetings
 
 class MeetingsSerializer(serializers.ModelSerializer):
+    student_firstname = serializers.CharField(source='student.firstname', read_only=True)
+    student_lastname = serializers.CharField(source='student.surname', read_only=True)
+    professor_firstname = serializers.CharField(source='professor.firstName', read_only=True)
+    professor_lastname = serializers.CharField(source='professor.surname', read_only=True)
+    year = serializers.SerializerMethodField()
+    month = serializers.SerializerMethodField()
+    day = serializers.SerializerMethodField()
+    time = serializers.SerializerMethodField()
+
     class Meta:
         model = Meetings
-        fields = 'student', 'professor', 'datetime', 'status', 'duration'
+        fields = ['student_firstname','student_lastname','professor_firstname', 'professor_lastname', 'year', 'month', 'day', 'time', 'jitsiLink','subject']
+
+    def get_year(self, obj):
+        return obj.datetime.year
+
+    def get_month(self, obj):
+        return obj.datetime.month
+
+    def get_day(self, obj):
+        return obj.datetime.day
+
+    def get_time(self, obj):
+        return obj.datetime.strftime('%H:%M:%S')  # Форматирование времени в часы:минуты:секунды
