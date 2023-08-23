01d5cd80bd6dc812de3bf213877480800c4dc635
AndreyPyBack <125370147+AndreyPyBack@users.noreply.github.com>
2023-08-23 09:32:53 +0300

    Update urls.py
    media

--- unibilim/urls.py
+++ unibilim/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 
@@ -17,3 +18,5 @@ urlpatterns = [
     # path('api/', include('payments.urls')),
     path('admin/', admin.site.urls),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
