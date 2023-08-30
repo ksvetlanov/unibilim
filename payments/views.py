7ef572e78263d06f27830c67bc81e41b1f816019
pythonMaks <122172870+pythonMaks@users.noreply.github.com>
2023-08-30 10:20:40 +0300

    Update views.py

--- payments/views.py
+++ payments/views.py
@@ -160,10 +160,10 @@ class InitiatePaymentView(APIView):
 
         # Вы можете реализовать проверку ответа здесь, если это необходимо
         response = requests.post('https://api.freedompay.money/init_payment.php', data=request)
-        # root = ET.fromstring(response)
-        # pg_redirect_url = root.find('pg_redirect_url').text
-        # return pg_redirect_url
-        return response  # Вы можете вернуть более полезные данные здесь, например response.json(), если это необходимо
+        root = ET.fromstring(response)
+        pg_redirect_url = root.find('pg_redirect_url').text
+        return pg_redirect_url
+        №return response  # Вы можете вернуть более полезные данные здесь, например response.json(), если это необходимо
 
 
 class PaymentsViewSet(viewsets.ModelViewSet):
