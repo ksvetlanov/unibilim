7253e415f546240e1ee318ab84db3f1d51aa74f4
pythonMaks <122172870+pythonMaks@users.noreply.github.com>
2023-12-25 10:31:52 +0300

    Update settings.py

--- unibilim/settings.py
+++ unibilim/settings.py
@@ -12,17 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-from dotenv import load_dotenv
 
-# Загрузка переменных окружения из файла .env
-load_dotenv()
-
-# Использование переменных окружения
-MERCHANT_ID = os.getenv('MERCHANT_ID')
-SECRET_KEY = os.getenv('SECRET_KEY')
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
