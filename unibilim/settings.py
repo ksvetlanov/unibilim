242d3cf25ab5b95af25598504ad958672dde986d
pythonMaks <122172870+pythonMaks@users.noreply.github.com>
2023-12-25 11:43:51 +0300

    Update settings.py

--- unibilim/settings.py
+++ unibilim/settings.py
@@ -149,33 +149,33 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-        'class': 'logging.StreamHandler',
-        },
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': 'debug.log',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console', 'file'],
-            'level': 'WARNING',  # Записывать только предупреждения и ошибки от Django
-            'propagate': True,
-        },
-        'payments': {  # Используйте имя вашего приложения или '__main__' для основного файла
-            'handlers': ['console', 'file'],
-            'level': 'INFO',  # Записывать сообщения уровня INFO и выше для вашего приложения
-            'propagate': False,
-        },
-    },
-}
-# settings.py
+#LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'handlers': {
+#         'console': {
+#         'class': 'logging.StreamHandler',
+#         },
+#         'file': {
+#             'level': 'DEBUG',
+#             'class': 'logging.FileHandler',
+#             'filename': 'debug.log',
+#         },
+#     },
+#     'loggers': {
+#         'django': {
+#             'handlers': ['console', 'file'],
+#             'level': 'WARNING',  # Записывать только предупреждения и ошибки от Django
+#             'propagate': True,
+#         },
+#         'payments': {  # Используйте имя вашего приложения или '__main__' для основного файла
+#             'handlers': ['console', 'file'],
+#             'level': 'INFO',  # Записывать сообщения уровня INFO и выше для вашего приложения
+#             'propagate': False,
+#         },
+#     },
+# }
+
 
 MEDIA_URL = '/media/'  # URL для медиа-файлов
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # Путь до папки с медиа-файлами
